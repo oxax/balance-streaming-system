ebd1561818906c6913e7bd33c4ba7d6fc6929e3b
Tobi Osagbemi <148675462+tobiosol@users.noreply.github.com>
2025-07-18 01:58:03 +0100

    feat: add initial README with problem summary and architectural intent

---
+++ src/main/java/com/jpc/exercise/BalanceStreamingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jpc.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceStreamingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceStreamingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpc/exercise/BalanceStreamingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpc.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceStreamingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
